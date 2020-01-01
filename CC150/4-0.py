18145d95fb5ae3a18ec0bc278aa5b8e3f9f2393b
max2 <mymimax@mi.com>
2020-01-02 00:11:56 +0900

    new year resolution!

---
+++ CC150/4-0.py
@@ -0,0 +1,7 @@
+#this is a testing file for chapter 4 containing simole and basic tree&graph algorithms
+def node():
+    def __init__(self,name):
+        self.name=name
+        self.children=[]
+
+print('hello,2020')
