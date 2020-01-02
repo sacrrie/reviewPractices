5cc3a89fd31fe4a8327dcefa9d90a05dfdaa1128
Yan, Guanyu <guanyu.yan@gatech.edu>
2020-01-02 23:57:42 +0900

    Tree node and In order traverse

--- CC150/4-0.py
+++ CC150/4-0.py
@@ -1,7 +1,20 @@
 #this is a testing file for chapter 4 containing simole and basic tree&graph algorithms
-def node():
-    def __init__(self,name):
-        self.name=name
-        self.children=[]
+class node:
+    def __init__(self,msg):
+        self.msg=msg
+        self.right=None
+        self.left=None
 
-print('hello,2020')
+#In order traverse
+def inorder(root):
+    if root is not None:
+        inorder(root.left)
+        print(root.msg)
+        inorder(root.right)
+#print('hello,2020')
+tree=node('ag')
+left=node('left')
+right=node('right')
+tree.left=left
+tree.right=right
+inorder(tree)
