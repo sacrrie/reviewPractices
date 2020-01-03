f5d90e9a26b276058e951525ef7d2ef587a187f3
Yan, Guanyu <guanyu.yan@gatech.edu>
2020-01-03 14:38:05 +0900

    finished tree, starting graph

--- CC150/4-0.py
+++ CC150/4-0.py
@@ -1,5 +1,5 @@
 #this is a testing file for chapter 4 containing simole and basic tree&graph algorithms
-class node:
+class binary_tree_node:
     def __init__(self,msg):
         self.msg=msg
         self.right=None
@@ -12,9 +12,40 @@ def inorder(root):
         print(root.msg)
         inorder(root.right)
 #print('hello,2020')
-tree=node('ag')
-left=node('left')
-right=node('right')
+#Pre order traverse
+def preorder(root):
+    if root is not None:
+        print(root.msg)
+        inorder(root.left)
+        inorder(root.right)
+#post order traverse
+def postorder(root):
+    if root is not None:
+        inorder(root.left)
+        inorder(root.right)
+        print(root.msg)
+#reverse order traverse
+def reverseorder(root):
+    if root is not None:
+        inorder(root.right)
+        print(root.msg)
+        inorder(root.left)
+tree=binary_tree_node('root')
+left=binary_tree_node('left')
+right=binary_tree_node('right')
 tree.left=left
 tree.right=right
 inorder(tree)
+preorder(tree)
+postorder(tree)
+reverseorder(tree)
+
+#undirected graph set up
+class graph:
+    def __init__(self):
+        self.list=[]
+
+class graph_node:
+    def __init__(self,msg):
+        self.msg=msg
+        self.neighbor=[]
