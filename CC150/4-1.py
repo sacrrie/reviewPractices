be4300a96eaad1d7a39f6aab9435876c97687ab3
Yan-hasee <sacrrie.yan@gmail.com>
2020-01-08 19:50:40 +0900

    official solution for 4-1 submitted

--- CC150/4-1.py
+++ CC150/4-1.py
@@ -4,6 +4,7 @@ def BFS(node):
     #return all the nodes it can reach in a set
     #idea for improvement: stop once when reaches target
     pass
+    return([None])
 
 def pathfinder(node1,node2):
     set1=BFS(node1)
@@ -13,3 +14,32 @@ def pathfinder(node1,node2):
     set2=BFS(node2)
     if node1 in set2:
         print("Node1 can be reached from node2")
+
+
+#the above is basically right, simple graph travelsal would do the trick,
+#DFS BFS all would work, textbook solution is given below:
+import queue
+states=enumerate('Unvisited', 'Visited', 'Visiting')
+def search(graph,start,end):
+    if start == end:
+        return True
+    q=queue.Queue()
+    for n in graph:
+        n.visited='Unvisited'
+    q.put(start)
+    while not q.empty():
+        u=q.get()
+        if u is not None:
+            for i in u.neighbor:
+                if i.state=='Unvisited':
+                    if i==end:
+                        return(True)
+                    else:
+                        i.state='Visited'
+                        q.put(i)
+        u.state='Visited'
+    return(False)
+
+#It is worth discussing with interviewer the tradeoffs between BFS and DFS.
+#DFS is easier to implement but BFS helps to find shortest path. DFS would go
+# very deeply before checking other immediate neighbors thus wasting efforts.
