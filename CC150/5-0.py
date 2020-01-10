960e68dfd82aa912531b76598c1999b94edb61c7
max2 <mymimax@mi.com>
2020-01-11 00:07:58 +0900

    arithmetic shift

--- CC150/5-0.py
+++ CC150/5-0.py
@@ -1,3 +1,11 @@
 #bit manipulation practice file
 #a simple bit manipulation function
 print(int('111001', 2))
+
+def repeated_arithmetic_right_shift(base,time):
+    answer=base
+    for i in range(time):
+        answer=base>>1
+    return(answer)
+a=repeated_arithmetic_right_shift(-75,1)
+print(a)
