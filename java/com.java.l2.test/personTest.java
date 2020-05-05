cd407b51b45c7c4c8147e59143395b5ebfceb653
Yan <guanyu.yan@gatech.edu>
2020-05-05 18:15:47 +0900

    start the java practice

---
+++ java/com.java.l2.test/personTest.java
@@ -0,0 +1,13 @@
+//this is the first java program for testing
+package com.java.l2.test;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+public class personTest {
+	@Test
+	public void returnTest(){
+		person yan = new person();
+		assertEquals("Hello World",yan.returnHello());
+	}
+
+
+}
